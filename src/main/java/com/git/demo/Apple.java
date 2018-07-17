d2c1e1bcf6aa795b57605c855ff0390cc03ac381
zzw <zzw@qq.com>
2018-07-17 19:44:40 +0800

    my first commit in eclipse

---
+++ src/main/java/com/git/demo/Apple.java
@@ -0,0 +1,19 @@
+package com.git.demo;
+
+public class Apple {
+	private String color;
+	private int price;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
